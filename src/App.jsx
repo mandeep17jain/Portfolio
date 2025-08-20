c2bf473caa3e2a69a91b1fb40c0e1afbded31e98
mandeep17jain <mandeepnalwaya@gmail.com>
2025-08-20 20:54:24 +0530

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -3,69 +3,69 @@ import Navbar from './components/Navbar'
 import Aboutme from './components/Aboutme'
 import './App.css'
 import Sidebar from './components/Sidebar'
-// import {BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider} from "react-router-dom"
-import { HashRouter,Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider} from "react-router-dom"
+// import { HashRouter,Routes, Route } from 'react-router-dom'
 import { Contact } from './components/Contact'
 import Resume from './components/Resume'
 import Portfolio from './components/Portfolio'
 // import ScrollToTop from './components/ScrollToTop'
 
 
-// const router=createBrowserRouter(
-//   [
-//     { path:"/", element:
-//     <div className='main-div'>
-//       <div className='second'>
-//         {/* <ScrollToTop /> */}
-//         <Aboutme/>
-//         <Sidebar/>
-//       </div>
-//     </div>
-//     },
+const router=createBrowserRouter(
+  [
+    { path:"/", element:
+    <div className='main-div'>
+      <div className='second'>
+        {/* <ScrollToTop /> */}
+        <Aboutme/>
+        <Sidebar/>
+      </div>
+    </div>
+    },
 
-//     { path:"/resume", element:
-//       <div className='main-div'>
+    { path:"/resume", element:
+      <div className='main-div'>
         
-//         <div className='second'>
-//           <Resume/>
-//           <Sidebar/>
-//         </div>
-//       </div>
-//       },
+        <div className='second'>
+          <Resume/>
+          <Sidebar/>
+        </div>
+      </div>
+      },
 
-//       { path:"/portfolio", element:
-//       <div className='main-div'>
+      { path:"/portfolio", element:
+      <div className='main-div'>
         
-//         <div className='second'>
-//           <Portfolio/>
-//           <Sidebar/>
-//         </div>
-//       </div>
-//       },
+        <div className='second'>
+          <Portfolio/>
+          <Sidebar/>
+        </div>
+      </div>
+      },
 
-//       { path:"/contact", element:
-//       <div className='main-div'>
-//         <div className='second'>
-//           <Contact/>
-//           <Sidebar/>
-//         </div>
-//       </div>
-//       }
+      { path:"/contact", element:
+      <div className='main-div'>
+        <div className='second'>
+          <Contact/>
+          <Sidebar/>
+        </div>
+      </div>
+      }
 
-//   ],
-  // {
-  //   basename:"/My-portfolio"
-  // }
-// )
+  ],
+  {
+    basename:"/My-portfolio"
+  }
+)
 
 function App() {
  
 
   return (
     <>
-    {/* <RouterProvider router={router}/> */}
+    <RouterProvider router={router}/>
 
-    <HashRouter>
+    {/* <HashRouter>
       <Routes>
         
         <Route
@@ -122,7 +122,7 @@ function App() {
         />
       </Routes>
     </HashRouter>
-    
+     */}
     </>
   )
 }
